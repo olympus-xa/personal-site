55baa0039221d185533f6965f11778b5f3573a86
Jacob L <jacob@hannahs-air-2.attlocal.net>
2023-07-30 14:54:39 -0400

    Add dropdown menu to poetry page.

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// home page slideshow
+
 let slideIndex = 0;
 showSlides();
 
@@ -19,4 +21,25 @@ function showSlides() {
   if (slideIndex > slides.length) {slideIndex = 1}
   slides[slideIndex-1].style.display = "block";
   setTimeout(showSlides, 5000); // Change image every 5 seconds
+} 
+
+// poem dropdown menu
+
+// show/hide menu on click 
+function myFunction() {
+  document.getElementById("poemDropdown").classList.toggle("show");
+}
+
+// close menu if user clicks outside menu
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-poems");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
 } 
